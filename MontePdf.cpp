21d71396c947a32cc01206782d53d5824e6571a8
Julio-Baeta1 <julbay1643@gmail.com>
2022-04-21 13:59:21 +0200

    Started extenstion to polynomial function for monte carlo points area ratio

--- MontePdf.cpp
+++ MontePdf.cpp
@@ -8,52 +8,92 @@
 using namespace arma;
 
 // A simple function to calculate area under straight line y=x using monte carlo method
-double monteStraightLine(double& lower, double& upper, int& n);
+double monteStraightLine(double& lower, double& upper, int& n, vec& coeffs);
 
 // Use transform density to uniform density to calculate area of y=x
 double monteUniformArea(double& lower, double& upper, int& n);
 
+//double polyYvalue(double& x, vec& coeffs);
+int interval_contained(vec& partition, double& val);
+
 
 int main()
 {
-	int n{ 100000 };
-	double low{ 0 }, up{ 16 }, ans{0};
+	int n{ 100 }; //10000
+	double low{ 0 }, up{ 1 }, ans{0};
+	vec straight_no_const("1 0");
 	
-	// Make hypothesis test to check number of points is sufficient
-	for (int i = 0; i < 10; i++)
+
+	//// Make hypothesis test to check number of points is sufficient
+	for (int i = 0; i < 2; i++)
 	{
-		ans = monteStraightLine(low, up, n);
+		ans = monteStraightLine(low, up, n, straight_no_const);
 		cout << ans << endl;
 	}
 
-	cout << endl << "Alternative test:" << endl;
-	for (int i = 0; i < 10; i++)
-	{
-		ans = monteUniformArea(low, up, n);
-		cout << ans << endl;
-	}
+	//cout << endl << "Alternative test:" << endl;
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	ans = monteUniformArea(low, up, n);
+	//	cout << ans << endl;
+	//}
 
 	return 0;
 }
 
-double monteStraightLine(double& lower, double& upper, int& n)
+int interval_contained(vec& partition, double& val)
+////test code
+//vec x = linspace(low, up, 9);
+//double val = 0.5;
+//int indx = interval_contained(x, val);
+//x.print("x:");
+//cout << "val: " << val << endl << "index: " << indx << endl;
+{
+	for (int i = 1; i < partition.n_elem+1; i++)
+	{
+		if (val <= partition.at(i))
+			return i;
+	}
+	
+	return 0;
+	// make code for when not in partition set
+}
+
+double monteStraightLine(double& lower, double& upper, int& n, vec& coeffs)
+//// coeffs [x^n, x^(n-1), ..., x^2, x, 1]
 // Add for lower =/= 0
 // Add for additional constant y = x + b
 // Add for different slope y = ax
 {
-	
-	double area = (upper-lower)* (upper - lower); //first test for lower = 0
+
+	//For lower = 0
+	double area = (upper-lower)* (upper - lower); 
+	double under_count{ 0 };
+	int indx{ 0 };
+
+	vec x = linspace(lower, upper, n);
+	vec y = polyval(coeffs, x);
+
 	mat A(n,2, fill::randu); 
 	A = A * (upper - lower);
-	double under{ 0 };
 
+	cout << "start" << endl;
 	for (int i = 0; i < n; i++)
 	{
-		if (A.at(i, 0) > A.at(i, 1)) // Since y=x
-			under++;
-	}
+		indx = interval_contained(x, A.at(i, 0));
 
-	area = area * under / n;
+		if (A.at(i, 1) <= y.at(indx))
+			under_count++;
+	}
+	cout << "end" << endl;
+
+	//for (int i = 0; i < n; i++)
+	//{
+		//if (A.at(i, 0) > A.at(i, 1)) // Since y=x
+		//	under++;
+	//}
+
+	area = area * under_count / n;
 
 	return area;
 }
